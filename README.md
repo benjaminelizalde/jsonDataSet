0cc6e792af8d6152934db4be89567a842dea6504
P3 <668292@my.sbunified.org>
2018-11-28 12:44:32 -0800

    added the dropdown made some progress on the return function

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # jsonDataSet
+
+I can graph the ranges the diseases existed and i can graph how many people each disease effected per year
+
+I can make a comparison for every disease in every year 
+
+I can make a page of information for each disease 
+
+For my MVP I want have a drop down with years and I want a list of all the diseases that were around that year
--- webapp.py
+++ webapp.py
@@ -1,6 +1,11 @@
-from flask import Flask, url_for, render_template, request
+from flask import Flask, url_for, render_template, request, Markup
+import os
+import json
 
 app = Flask(__name__)
+
+with open('health.json') as health_data:
+    diseases = json.load(health_data)
 
 @app.route("/")
 def render_main():
@@ -8,7 +13,10 @@ def render_main():
 
 @app.route("/p1")
 def render_page1():
-    return render_template('page1.html')
+    if "States" not in request.args:
+        return render_template('page1.html', disease = disease_lifetime())
+    else:
+        return render_template('page1.html', disease = disease_lifetime(), start = start(request.args["disease"]) )
 
 @app.route("/p2")
 def render_page2():
@@ -18,7 +26,26 @@ def render_page2():
 def render_page3():
     return render_template('page3.html')
     
+    
+    
+    
+def disease_lifetime():
+    listOfDiseases = []
+     for x in diseases:
+        if not x["disease"] in listOfDiseases:
+            listOfDiseases.append(x["disease"])
+     start = ""
+     for x in listOfDiseases:
+        start = start +  Markup("<option value=\"" + x + "\">" + x + "</option>")
+     return start 
+    
 
+    
+def     first = counties[0]["Age"]["Percent Under 18 Years"]
+    for county in counties:
+        if county["Age"]["Percent Under 18 Years"] < first:
+            first = county["Age"]["Percent Under 18 Years"]
+    return first
     
  
 if __name__=="__main__":
