6f52a9ecba3f97ef607aa1850abb1f82468ea4be
P3 <668292@my.sbunified.org>
2018-12-05 12:39:07 -0800

    h

--- webapp.py
+++ webapp.py
@@ -17,7 +17,7 @@ def render_page1():
         return render_template('page1.html', disease = disease_lifetime())
     else:
         chosen_disease = request.args["disease"]
-        return render_template('page1.html', disease = disease_lifetime(), start = disease_start(request.args["disease"]) )
+        return render_template('page1.html', disease = disease_lifetime(), startlast = str(disease_start(chosen_disease)) +"-"+ str(disease_end(chosen_disease)))
 
 @app.route("/p2")
 def render_page2():
@@ -44,10 +44,18 @@ def disease_lifetime():
     
 def  disease_start(chosen_disease):   
     first = 3000
+    last = 0
     for x in diseases:
-        if x["year"] < first:
+        if x["year"] < first and chosen_disease == x["disease"]:
             first = x["year"]
     return first
+    
+def  disease_end(chosen_disease):   
+    last = 0
+    for x in diseases:
+        if x["year"] > last and chosen_disease == x["disease"]:
+            last = x["year"]
+    return last
     
  
 if __name__=="__main__":
